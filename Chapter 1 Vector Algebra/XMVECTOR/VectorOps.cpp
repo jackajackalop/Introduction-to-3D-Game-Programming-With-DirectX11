3381fe6cc050206b3d7b97212db732e795645b86
Yixin He <yixinhe98@gmail.com>
2023-02-01 14:57:29 -0800

    Fix ch1 build

--- Chapter 1 Vector Algebra/XMVECTOR/VectorOps.cpp
+++ Chapter 1 Vector Algebra/XMVECTOR/VectorOps.cpp
@@ -1,14 +1,14 @@
 
 #include <windows.h> // for FLOAT definition
-#include <xnamath.h>
+#include <DirectXMath.h>
 #include <iostream>
 using namespace std;
 
 // Overload the  "<<" operators so that we can use cout to 
 // output XMVECTOR objects.
-ostream& operator<<(ostream& os, FXMVECTOR v)
+ostream& operator<<(ostream& os, DirectX::FXMVECTOR v)
 {
-	XMFLOAT4 dest;
+	DirectX::XMFLOAT4 dest;
 	XMStoreFloat4(&dest, v);
 
 	os << "(" << dest.x << ", " << dest.y << ", " << dest.z <<  ", " << dest.w << ")";
@@ -20,35 +20,35 @@ int main()
 	cout.setf(ios_base::boolalpha);
 
 	// Check support for SSE2 (Pentium4, AMD K8, and above).
-	if( !XMVerifyCPUSupport() )
+	if( !DirectX::XMVerifyCPUSupport() )
 	{
 		cout << "xna math not supported" << endl;
 		return 0;
 	}
 	
-	XMVECTOR p = XMVectorSet(2.0f, 2.0f, 1.0f, 0.0f);
-	XMVECTOR q = XMVectorSet(2.0f, -0.5f, 0.5f, 0.1f);
-	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 4.0f, 8.0f);
-	XMVECTOR v = XMVectorSet(-2.0f, 1.0f, -3.0f, 2.5f);
-	XMVECTOR w = XMVectorSet(0.0f, XM_PIDIV4, XM_PIDIV2, XM_PI);
+	DirectX::XMVECTOR p = DirectX::XMVectorSet(2.0f, 2.0f, 1.0f, 0.0f);
+	DirectX::XMVECTOR q = DirectX::XMVectorSet(2.0f, -0.5f, 0.5f, 0.1f);
+	DirectX::XMVECTOR u = DirectX::XMVectorSet(1.0f, 2.0f, 4.0f, 8.0f);
+	DirectX::XMVECTOR v = DirectX::XMVectorSet(-2.0f, 1.0f, -3.0f, 2.5f);
+	DirectX::XMVECTOR w = DirectX::XMVectorSet(0.0f, DirectX::XM_PIDIV4, DirectX::XM_PIDIV2, DirectX::XM_PI);
 
-	cout << "XMVectorAbs(v)                 = " << XMVectorAbs(v) << endl;
-	cout << "XMVectorCos(w)                 = " << XMVectorCos(w) << endl;
-	cout << "XMVectorLog(u)                 = " << XMVectorLog(u) << endl;
-	cout << "XMVectorExp(p)                 = " << XMVectorExp(p) << endl;
+	cout << "XMVectorAbs(v)                 = " << DirectX::XMVectorAbs(v) << endl;
+	cout << "XMVectorCos(w)                 = " << DirectX::XMVectorCos(w) << endl;
+	cout << "XMVectorLog(u)                 = " << DirectX::XMVectorLog(u) << endl;
+	cout << "XMVectorExp(p)                 = " << DirectX::XMVectorExp(p) << endl;
 	
-	cout << "XMVectorPow(u, p)              = " << XMVectorPow(u, p) << endl;
-	cout << "XMVectorSqrt(u)                = " << XMVectorSqrt(u) << endl;
+	cout << "XMVectorPow(u, p)              = " << DirectX::XMVectorPow(u, p) << endl;
+	cout << "XMVectorSqrt(u)                = " << DirectX::XMVectorSqrt(u) << endl;
 	
 	cout << "XMVectorSwizzle(u, 2, 2, 1, 3) = " 
-		<< XMVectorSwizzle(u, 2, 2, 1, 3) << endl;
+		<< DirectX::XMVectorSwizzle(u, 2, 2, 1, 3) << endl;
 	cout << "XMVectorSwizzle(u, 2, 1, 0, 3) = " 
-		<< XMVectorSwizzle(u, 2, 1, 0, 3) << endl;
+		<< DirectX::XMVectorSwizzle(u, 2, 1, 0, 3) << endl;
 
-	cout << "XMVectorMultiply(u, v)         = " << XMVectorMultiply(u, v) << endl;
-	cout << "XMVectorSaturate(q)            = " << XMVectorSaturate(q) << endl;
-	cout << "XMVectorMin(p, v               = " << XMVectorMin(p, v) << endl;
-	cout << "XMVectorMax(p, v)              = " << XMVectorMax(p, v) << endl;
+	cout << "XMVectorMultiply(u, v)         = " << DirectX::XMVectorMultiply(u, v) << endl;
+	cout << "XMVectorSaturate(q)            = " << DirectX::XMVectorSaturate(q) << endl;
+	cout << "XMVectorMin(p, v               = " << DirectX::XMVectorMin(p, v) << endl;
+	cout << "XMVectorMax(p, v)              = " << DirectX::XMVectorMax(p, v) << endl;
 
 	return 0;
 }
